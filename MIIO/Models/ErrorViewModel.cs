76a4d8e4065024acf4532fdb9b77333439002c3d
Keint Rojas <keintrojas@gmail.com>
2025-11-10 15:18:35 -0600

    Agregar archivos de proyecto.

---
+++ MIIO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MIIO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
