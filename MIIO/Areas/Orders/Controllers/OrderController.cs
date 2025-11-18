d3b938c85ada683b0e89b84b16279ecbb3cfd394
Keint Rojas <keintrojas@gmail.com>
2025-11-18 09:23:01 -0600

    actualización rápida

---
+++ MIIO/Areas/Orders/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIIO.Areas.Orders.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
