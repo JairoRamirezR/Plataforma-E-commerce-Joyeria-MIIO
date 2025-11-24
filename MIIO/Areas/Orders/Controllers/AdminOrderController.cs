bc457792360a2167f715df3b7fdaf064c19de356
Keint Rojas <keintrojas@gmail.com>
2025-11-24 14:45:10 -0600

    Carrito

--- MIIO/Areas/Orders/Controllers/AdminOrderController.cs
+++ MIIO/Areas/Orders/Controllers/AdminOrderController.cs
@@ -22,22 +22,7 @@ namespace MIIO.Areas.Orders.Controllers
         {
             return View();
         }
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Create(Order order)
-        {
-            _unitOfWork.Order.Add(order);
-
-            _unitOfWork.Save();
-
-            TempData["success"] = "Orden almacenada correctamente";
-
-            return RedirectToAction("Index");
-        }
+        
         public IActionResult Edit(int? id)
         {
             if (id == null || id == 0)
---
+++ MIIO/wwwroot/js/cart.js
@@ -0,0 +1,58 @@
+﻿function removeFromCart(id) {
+    $.ajax({
+        url: `/Orders/Cart/Remove/${id}`,
+        type: "POST",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+
+                // Recargar el carrito si estás en esa página
+                location.reload();
+            } else {
+                toastr.error(data.message);
+            }
+        },
+        error: function () {
+            toastr.error("Error al eliminar del carrito");
+        }
+    });
+}
+function addToCart(productId) {
+
+    $.ajax({
+        url: `/Orders/Cart/Add/${productId}`,
+        type: "POST",
+        success: function (data) {
+            if (data.success) {
+                toastr.success("Producto agregado al carrito");
+            } else {
+                toastr.error("No se pudo agregar al carrito");
+            }
+        },
+        error: function () {
+            toastr.error("Error al conectar con el servidor");
+        }
+    });
+}
+function processPayment() {
+    $.ajax({
+        url: "/Orders/Order/Create",
+        type: "POST",
+        success: function (res) {
+            if (res.success) {
+                toastr.success("Orden generada correctamente");
+
+                // Redirigir 
+                setTimeout(() => {
+                    window.location.href = "/Orders/Order/Details/" + res.orderId;
+                }, 1000);
+
+            } else {
+                toastr.error("No se pudo completar el pago");
+            }
+        },
+        error: function () {
+            toastr.error("Error al procesar el pago");
+        }
+    });
+}
